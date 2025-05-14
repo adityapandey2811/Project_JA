b4bf1b08adb86e99177d35206a0fa449f151bf43
Aditya Pandey <2811adityapandey@gmail.com>
2025-05-14 22:24:45 +0530

    Initial Commit

---
+++ src/main/java/com/ja/Project_JA/ProjectJaApplication.java
@@ -0,0 +1,13 @@
+package com.ja.Project_JA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ja/Project_JA/ProjectJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ja.Project_JA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
